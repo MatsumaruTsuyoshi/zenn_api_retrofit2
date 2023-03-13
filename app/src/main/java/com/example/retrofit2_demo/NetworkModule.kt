7c5c15329ad2c884c3900c67d52e8db1076446a8
MatsumaruTsuyoshi <strivesdfpx379@gmail.com>
2023-03-13 17:19:10 +0900

    feat: create module

---
+++ app/src/main/java/com/example/retrofit2_demo/NetworkModule.kt
@@ -0,0 +1,27 @@
+package com.example.retrofit2_demo
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://zenn-api.vercel.app/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+}
